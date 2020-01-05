65b137bf53d18c3446af4f7bf4a72d0488e1435f
Zakhar Osokin <jaguar7894@yandex.ru>
2020-01-05 16:07:55 +0300

    Add files via upload

---
+++ csc-java/week_02/poweroftwo/PowerOfTwo.java
@@ -0,0 +1,25 @@
+/* 2.2
+Реализуйте метод, проверяющий, является ли заданное число по абсолютной величине степенью двойки.
+Решать можно разными способами:
+- воспользовавшись одним удобным статическим методом из класса java.lang.Integer;
+- применив пару трюков из двоичной арифметики;
+- написав решение "в лоб" с циклом и условными операторами (можете вернуться к этой задаче после просмотра соответствующих уроков).
+*/
+
+public class PowerOfTwo {
+    /**
+     * Checks if given <code>value</code> is a power of two.
+     *
+     * @param value any number
+     * @return <code>true</code> when <code>value</code> is power of two, <code>false</code> otherwise
+     */
+    public static boolean isPowerOfTwo(int value) {
+        return Integer.bitCount(Math.abs(value)) == 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(0));
+        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwo(-2));
+    }
+}
